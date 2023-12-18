40ed95a4eb06ed465819243d27b3b12b7581eb5e
w0lwin <teo.bacher@hotmail.com>
2023-12-18 15:50:12 +0100

    feat(controller) add Admin page

---
+++ src/Front/Component/Admin.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { Paper, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import axios from 'axios';
+
+const AdminPage = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/Utilisateurs');
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Paper>
+      <Typography variant="h4" gutterBottom>
+        Page Admin
+      </Typography>
+
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID Utilisateur</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Prénom</TableCell>
+              <TableCell>Nom</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userData.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.prenom}</TableCell>
+                <TableCell>{user.nom}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
+};
+
+export default AdminPage;
