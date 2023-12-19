8e0f75f98d456d5ce7a5d8e6d6e576cda024853e
redes19 <120016236+redes19@users.noreply.github.com>
2023-12-19 11:29:01 +0100

    feat(controller): update carteDAO.php add crud

--- src/Back/PHP/DAO/carteDAO.php
+++ src/Back/PHP/DAO/carteDAO.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'carte.php';
+
 class CarteDAO {
     private $db;
 
@@ -7,36 +9,73 @@ class CarteDAO {
         $this->db = $db;
     }
 
-    // Create
-    public function create(Carte $carte){
-        $query = "INSERT INTO cartes (id, name, description, imageUrl, race, type, frameType, cardId) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param("issssssi", $carte->getId(), $carte->getName(), $carte->getDesc(), $carte->getImageUrl(), $carte->getRace(), $carte->getType(), $carte->getFrameType(), $carte->getCardId());
-        
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
+    public function getCartes(){
+        $cartes = array();
+
+        $query = "SELECT * FROM cartes";
+        $result = $this->db->query($query);
+
+        while ($row = $result->fetch_assoc()) {
+            $carte = new Carte(
+                $row['id'],
+                $row['name'],
+                $row['description'],
+                $row['imageUrl'],
+                $row['race'],
+                $row['type'],
+                $row['frameType'],
+                $row['cardId']
+            );
+
+            $cartes[] = $carte;
         }
+
+        return $cartes;
     }
 
-    // Read
-    public function getById($id){
-        $query = "SELECT * FROM cartes WHERE id = ?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param("i", $id);
+    public function getCarteById($id){
+        $stmt = $this->pdo->prepare("SELECT * FROM carte WHERE id = :id");
+        $stmt->bindParam(':id', $id);
         $stmt->execute();
-        $result = $stmt->get_result();
-        $carte = $result->fetch_object("Carte");
-        return $carte;
-    }
+    
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+        if ($result) {
+            $carte = new Carte(
+                $result['id'],
+                $result['name'],
+                $result['desc'],
+                $result['imageUrl'],
+                $result['race'],
+                $result['type'],
+                $result['frameType'],
+                $result['cardId']
+            );
+            return $carte;
+        } else {
+            return null; 
+        }
+    }    
 
-    // Update
-    public function update(Carte $carte){
-        $query = "UPDATE cartes SET name = ?, description = ?, imageUrl = ?, race = ?, type = ?, frameType = ?, cardId = ? WHERE id = ?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param("ssssssii", $carte->getName(), $carte->getDesc(), $carte->getImageUrl(), $carte->getRace(), $carte->getType(), $carte->getFrameType(), $carte->getCardId(), $carte->getId());
-        
+    public function addCarte(Carte $carte){
+        $name = $carte->getName();
+        $desc = $carte->getDesc();
+        $imageUrl = $carte->getImageUrl();
+        $race = $carte->getRace();
+        $type = $carte->getType();
+        $frameType = $carte->getFrameType();
+        $cardId = $carte->getCardId();
+    
+        $stmt = $this->pdo->prepare("INSERT INTO carte (name, desc, imageUrl, race, type, frameType, cardId) VALUES (:name, :desc, :imageUrl, :race, :type, :frameType, :cardId)");
+    
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':desc', $desc);
+        $stmt->bindParam(':imageUrl', $imageUrl);
+        $stmt->bindParam(':race', $race);
+        $stmt->bindParam(':type', $type);
+        $stmt->bindParam(':frameType', $frameType);
+        $stmt->bindParam(':cardId', $cardId);
+    
         if ($stmt->execute()) {
             return true;
         } else {
@@ -44,18 +83,46 @@ class CarteDAO {
         }
     }
 
-    // Delete
-    public function delete($id){
-        $query = "DELETE FROM cartes WHERE id = ?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param("i", $id);
+    public function updateCarte(Carte $carte){
+        $id = $carte->getId();
+        $name = $carte->getName();
+        $desc = $carte->getDesc();
+        $imageUrl = $carte->getImageUrl();
+        $race = $carte->getRace();
+        $type = $carte->getType();
+        $frameType = $carte->getFrameType();
+        $cardId = $carte->getCardId();
+
+        $stmt = $this->pdo->prepare("UPDATE carte SET name = :name, des = :desc, imageUrl = :imageUrl, race = :race, type = :type, frameType = :frameType, cardId = :cardId WHERE id = :id");
+
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':desc', $desc);
+        $stmt->bindParam(':imageUrl', $imageUrl);
+        $stmt->bindParam(':race', $race);
+        $stmt->bindParam(':type', $type);
+        $stmt->bindParam(':frameType', $frameType);
+        $stmt->bindParam(':cardId', $cardId);
+        $stmt->execute();
+    }
+
+    public function deleteCarte(Carte $carte){
+        $id = $carte->getId();
+    
+        $stmt = $this->pdo->prepare("DELETE FROM carte WHERE id = :id");
+    
+        $stmt->bindParam(':id', $id);
         
+        // Exécution de la requête
         if ($stmt->execute()) {
             return true;
         } else {
             return false;
         }
     }
+    
+
+
 }
 
 
