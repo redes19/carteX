9d47928b418237427e077092fdc084c5f4e61ff3
redes19 <120016236+redes19@users.noreply.github.com>
2023-12-21 16:01:34 +0100

    docs(view): add commentaire

--- src/Back/PHP/DAO/carteDAO.php
+++ src/Back/PHP/DAO/carteDAO.php
@@ -5,16 +5,22 @@ require_once '../Class/carte.php';
 class CarteDAO {
     private $pdo;
 
+    // Constructor to initialize the PDO object
     public function __construct($pdo){
         $this->pdo = $pdo;
     }
 
+    // Method to retrieve all cartes from the database
     public function getCartes(){
         $cartes = array();
     
+        // SQL query to select all columns from the Carte table
         $query = "SELECT * FROM Carte";
+        
+        // Execute the query
         $result = $this->pdo->query($query);
     
+        // Loop through the result set and create Carte objects
         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
             $carte = new Carte(
                 $row['id'],
@@ -26,21 +32,31 @@ class CarteDAO {
                 $row['cardId']
             );
     
+            // Add the Carte object to the $cartes array
             $cartes[] = $carte;
         }
     
+        // Return the array of Carte objects
         return $cartes;
     }
     
 
     public function getCarteById($id){
+        // Prepare a SQL statement with a named parameter
         $stmt = $this->pdo->prepare("SELECT * FROM Carte WHERE id = :id");
+        
+        // Bind the parameter ':id' to the provided $id
         $stmt->bindParam(':id', $id);
+        
+        // Execute the prepared statement
         $stmt->execute();
-    
+        
+        // Fetch the result as an associative array
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    
+        
+        // Check if a result was obtained
         if ($result) {
+            // If a result exists, create a new Carte object
             $carte = new Carte(
                 $result['id'],
                 $result['name'],
@@ -50,53 +66,79 @@ class CarteDAO {
                 $result['type'],
                 $result['cardId']
             );
+            
+            // Return the Carte object
             return $carte;
         } else {
+            // If no result is found, return null
             return null; 
         }
-    } 
+    }
+     
     
     public function getCarteIdByName($carteName){
+        // Prepare a SQL statement with a named parameter
         $stmt = $this->pdo->prepare("SELECT id FROM Carte WHERE name = :name");
+        
+        // Bind the parameter ':name' to the provided $carteName
         $stmt->bindParam(':name', $carteName);
+        
+        // Execute the prepared statement
         $stmt->execute();
-    
+        
+        // Fetch the result as an associative array
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    
+        
+        // Check if a result was obtained
         if ($result) {
+            // If a result exists, return the 'id' value
             return $result['id'];
         } else {
+            // If no result is found, return null
             return null;
         }
     }
+    
 
     public function getCarteIdByCardId($cardId){
+        // Prepare a SQL statement with a named parameter
         $stmt = $this->pdo->prepare("SELECT id FROM Carte WHERE cardId = :cardId");
+        
+        // Bind the parameter ':cardId' to the provided $cardId
         $stmt->bindParam(':cardId', $cardId);
+        
+        // Execute the prepared statement
         $stmt->execute();
-    
+        
+        // Fetch the result as an associative array
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    
+        
+        // Check if a result was obtained
         if ($result) {
+            // If a result exists, return the 'id' value
             return $result['id'];
         } else {
+            // If no result is found, return null
             return null;
         }
     }
+    
     
 
     public function addCarte(Carte $card){        
+        // Define the base query and values for insertion
         $query = "INSERT INTO Carte (name, `desc`, imageUrl, race, type, cardId";
         $values = "VALUES (?, ?, ?, ?, ?, ?";
         $array = [
-          $card->getName(), 
-          $card->getDesc(),
-          $card->getImageUrl(),
-          $card->getRace(),
-          $card->getType(),
-          $card->getCardId(),
-            ];
-        
+            $card->getName(), 
+            $card->getDesc(),
+            $card->getImageUrl(),
+            $card->getRace(),
+            $card->getType(),
+            $card->getCardId(),
+        ];
+    
+        // Check and add optional attributes and prices
         if($card->getAmazonPrice() != null){
             $query .= ", amazonPrice";
             $values .= ", ?";
@@ -147,24 +189,30 @@ class CarteDAO {
             $values .= ", ?";
             array_push($array, $card->getLevel());
         }
-
+    
+        // Complete the query and values
         $query .= ") ";
         $values .= ")";
         $finalQuery = $query . $values;
-
+    
+        // Get the PDO connection
         $conn = $this->pdo;
-
+    
+        // Prepare and execute the query with the provided values
         $resultInsert = $conn->prepare($finalQuery);
         $resultInsert->execute($array);
         
+        // Check the result of the insertion
         if ($resultInsert) {
-            return true;
+            return true; // Insertion successful
         } else {
-            return false;
+            return false; // Insertion failed
         }
     }
+    
 
     public function updateCarte(Carte $carte){
+        // Extract attributes from the provided Carte object
         $id = $carte->getId();
         $name = $carte->getName();
         $desc = $carte->getDesc();
@@ -172,9 +220,11 @@ class CarteDAO {
         $race = $carte->getRace();
         $type = $carte->getType();
         $cardId = $carte->getCardId();
-
+    
+        // Prepare and execute the UPDATE query
         $stmt = $this->pdo->prepare("UPDATE Carte SET name = :name, `desc` = :desc, imageUrl = :imageUrl, race = :race, type = :type, cardId = :cardId WHERE id = :id");
-
+    
+        // Bind parameters
         $stmt->bindParam(':id', $id);
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':desc', $desc);
@@ -182,23 +232,30 @@ class CarteDAO {
         $stmt->bindParam(':race', $race);
         $stmt->bindParam(':type', $type);
         $stmt->bindParam(':cardId', $cardId);
+    
+        // Execute the query
         $stmt->execute();
     }
+    
 
     public function deleteCarte(Carte $carte){
+        // Extract the ID from the provided Carte object
         $id = $carte->getId();
     
+        // Prepare the DELETE query
         $stmt = $this->pdo->prepare("DELETE FROM Carte WHERE id = :id");
     
+        // Bind the ID parameter
         $stmt->bindParam(':id', $id);
         
-        // Exécution de la requête
+        // Execute the query
         if ($stmt->execute()) {
             return true;
         } else {
             return false;
         }
     }
+    
 }
 
 
